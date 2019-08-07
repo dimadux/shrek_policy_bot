76a939c64b2ca99398e6e633b5646a1b97c468e6
Dukh Dmytro <dimadux99@gmail.com>
2019-08-07 17:27:46 +0300

    bug fix

--- utils/db.py
+++ utils/db.py
@@ -92,4 +92,8 @@ class DB:
         else:
             registered_words.append(bad_word)
             self._get_coll("users").update_one({"_id": user_id}, {"$set": {"registered_words": registered_words}})
+            self._insert_doc("bad_words", {
+                "_id":bad_word,
+                "confidence":0.4
+            })
             return f"Слово {bad_word} успешно обновлено в списке плохих слов"
